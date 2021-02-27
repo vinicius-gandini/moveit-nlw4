e4386d494d467e5e8aa188c4ba0594155eb20223
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2021-02-27 15:31:36 -0300

    feat: :ok_hand: challenges context

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, useState, ReactNode } from 'react';
+
+interface ChallengesContextData {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
+  const[level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setCHallengesCompleted] = useState(0);
+
+  function levelUp() {
+    setLevel( + 1);
+  }
+
+  function startNewChallenge() {
+
+  }
+
+  return (
+    <ChallengesContext.Provider value={{
+      level,
+      currentExperience,
+      challengesCompleted,
+      levelUp,
+      startNewChallenge,
+    }}>
+      {children}
+    </ChallengesContext.Provider>
+  )
+}
